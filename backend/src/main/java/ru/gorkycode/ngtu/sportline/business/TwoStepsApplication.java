20f5cd5c5cff15799cf153bfb7cb99e1e64212ba
Ap1kkk <apikmaster2@yandex.ru>
2026-03-28 16:37:00 +0300

    fix Исправлен запуск бэкенда

--- backend/src/main/java/ru/gorkycode/ngtu/sportline/business/TwoStepsApplication.java
+++ backend/src/main/java/ru/gorkycode/ngtu/sportline/business/TwoStepsApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TwoStepsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SportLineApplication.class, args);
+        SpringApplication.run(TwoStepsApplication.class, args);
     }
 
 }
